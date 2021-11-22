d1a4b9182644e945ced2c809c0897d017b4ab7f8
yichuanz <yichuanz@amazon.com>
2021-11-22 19:44:59 +0800

    change 090803

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App TMD</h1>
-                <h2>Congratulationszzzzzzz</h2>
+                <h2>Congratulations1122</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
